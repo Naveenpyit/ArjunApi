994fac91501d82c9fa18c0931c3cb4c3b7dfb20e
NAVEENKUMAR M. <76612@TCS.Local>
2026-03-26 16:43:35 +0530

    small changes

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,13 +3,19 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 from django.contrib.auth import authenticate
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,AllowAny
 from .authentication import CookieAuthentication
 from . import models
 from . import serializers
 from django.utils import timezone
 
 class UserViews(APIView):
+    authentication_classes = [CookieAuthentication]
+
+    def get_permissions(self):
+        if self.request.method == 'POST':
+            return [AllowAny()]
+        return [IsAuthenticated()]
 
     def get(self,request,pk=None):
         try:
